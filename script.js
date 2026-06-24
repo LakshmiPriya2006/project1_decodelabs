f821dd02712f477ed9ada488bbe4d243938300ce
LakshmiPriya2006 <lakshmipriya.v2006@gmail.com>
2026-06-24 15:44:13 +0530

    Week 1 Assignment Submission

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeTheme(){
+
+    if(document.body.style.backgroundColor === "black"){
+        document.body.style.backgroundColor = "#f5f5f5";
+        document.body.style.color = "#333";
+    }
+    else{
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+    }
+
+}
